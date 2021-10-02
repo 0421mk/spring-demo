1f129d80c7ddaeb95b5cbddad6919a2220ae0754
youngbamer <0421mk@naver.com>
2021-10-03 00:25:25 +0900

    main2,3,4 추가

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -8,8 +8,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class UsrHomeController {
 	@RequestMapping("/usr/home/main")
 	@ResponseBody
-	
 	public String showMain() {
 		return "안녕하세요.";
+	}
+	
+	@RequestMapping("/usr/home/main2")
+	@ResponseBody
+	public String showMain2() {
+		return "안녕하세요2.";
+	}
+	
+	@RequestMapping("/usr/home/main3")
+	@ResponseBody
+	public String showMain3() {
+		return "안녕하세요3.";
+	}
+	
+	@RequestMapping("/usr/home/main4")
+	@ResponseBody
+	public String showMain4() {
+		return "안녕하세요4.";
 	}
 }
