f28b5411d3419f1d82aa8b24491f51e27b7210ac
youngbamer <0421mk@naver.com>
2021-10-03 01:32:59 +0900

    게시물 컨트롤러에 게시물 추가, 리스트 기능 구현

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.demo.vo.Article;
+
 import lombok.Data;
 
 @Controller
@@ -60,27 +62,4 @@ public class UsrHomeController {
 		// 출력 : ["철수","영희"]
 	}
 	
-	@RequestMapping("/usr/home/getArticle")
-	@ResponseBody
-	public Article getArticle() {
-		Article article = new Article();
-		
-		return article;
-	}
-}
-
-@Data
-class Article {
-	private int id;
-	private String title;
-	// 객체 Article 출력시 public을 써야 하지만 스프링에서는 private 권장
-	// 그래서 private 변수 위에 @Getter 를 사용하면 출력 가능
-	// 하지만 lombok을 class 위에 @Data로 사용하면 간편하게 사용가능
-	// @AllArgsConstructor => 생성자에 인자를 받을 수 있음
-	// @NoArgsConstructor => 생성자가 없는 클래스도 받을 수 있음
-	
-	public Article() {
-		id = 1;
-		title = "제목1";
-	}
 }
---
+++ src/main/java/com/example/demo/vo/Article.java
@@ -0,0 +1,19 @@
+package com.example.demo.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Article {
+	private int id;
+	private String title;
+	private String body;
+	// 객체 Article 출력시 public을 써야 하지만 스프링에서는 private 권장
+	// 그래서 private 변수 위에 @Getter 를 사용하면 출력 가능
+	// 하지만 lombok을 class 위에 @Data로 사용하면 간편하게 사용가능
+	// @AllArgsConstructor => 생성자에 인자를 받을 수 있음
+	// @NoArgsConstructor => 생성자가 없는 클래스도 받을 수 있음
+}
