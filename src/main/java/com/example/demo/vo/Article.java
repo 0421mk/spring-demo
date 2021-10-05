575062c27b0e8689014d8e091128b02b24038c73
youngbamer <0421mk@naver.com>
2021-10-06 01:15:57 +0900

    Article 클래스에 regDate, updateDate 추가

--- src/main/java/com/example/demo/vo/Article.java
+++ src/main/java/com/example/demo/vo/Article.java
@@ -11,6 +11,8 @@ public class Article {
 	private int id;
 	private String title;
 	private String body;
+	private String regDate;
+	private String updateDate;
 	// 객체 Article 출력시 public을 써야 하지만 스프링에서는 private 권장
 	// 그래서 private 변수 위에 @Getter 를 사용하면 출력 가능
 	// 하지만 lombok을 class 위에 @Data로 사용하면 간편하게 사용가능
