7ecadd59a7c2c63892fbd167360945256aeeb83b
youngbamer <0421mk@naver.com>
2021-10-07 14:03:14 +0900

    BeforeAction 인터셉터 대표로 한번 생성

--- src/main/java/com/example/demo/interceptor/BeforeActionInterceptor.java
+++ src/main/java/com/example/demo/interceptor/BeforeActionInterceptor.java
@@ -13,6 +13,7 @@ public class BeforeActionInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
 		Rq rq = new Rq(req);
+
 		req.setAttribute("rq", rq);
 		
 		return HandlerInterceptor.super.preHandle(req, resp, handler);
